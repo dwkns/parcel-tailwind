ed08c2578d6b3ab2198292261359ed811d19ebe6
dwkns <dwkns@me.com>
2020-07-13 20:58:46 +0100

    Updated config for tailwind 1.4 — improved html component examples

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
-const lastElement = document.body.lastChild ;
-const paragraph = document.createElement('p');
-paragraph.classList.add('text-center', 'text-green-600'); 
-paragraph.innerText = 'This paragraph was added from JS to show bundling is working. Tailwind is working if it\'s green';
-document.body.insertBefore(paragraph, lastElement);
+console.log('hello');
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log('DOMContentLoaded');
+
+
+  var thisDiv = document.getElementById("hmtl-div");
+  console.log(thisDiv);
+  // thisDiv.className = '';
+  thisDiv.addEventListener("click", colourToggle);
+
+ function colourToggle() {
+  console.log('colourToggle clicked');
+  var thisDiv = document.getElementById("hmtl-div");
+    if ( thisDiv.classList.contains('bg-green-400') ) {
+      thisDiv.classList.replace("bg-green-400", "bg-red-400");
+    } else {
+      thisDiv.classList.replace("bg-red-400","bg-green-400");
+    }
+ }
+});
